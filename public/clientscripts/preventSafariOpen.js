8b1ff9d28a369a3d4429860dd59a84fb7dfc7222
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-05-28 15:26:36 +0200

    Fixing broken navigation in iOS devices. Preventing links from being opened in Safari when it should be opened in the application.

--- public/clientscripts/preventSafariOpen.js
+++ public/clientscripts/preventSafariOpen.js
@@ -5,11 +5,13 @@
  * @version 1.0.0
  */
 
-let a = document.getElementsByTagName('a');
+ (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener('click',function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;'href'in d&&(d.href.indexOf('http')||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href);},!1);}})(document,window.navigator,'standalone');
+
+/*let a = document.getElementsByTagName('a');
 for (let x = 0; x < a.length; x += 1)
 {
     a[x].addEventListener('click', function() {
         window.location = this.getAttribute('href');
         return false;
     });
-}
+}*/
