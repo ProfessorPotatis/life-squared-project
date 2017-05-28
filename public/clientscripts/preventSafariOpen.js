2c24671ad91f5529e1041dc9c849bdaaeeeae1da
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-05-28 15:30:56 +0200

    Navigation in iOS devices fixed. Preventing links to be opened in safari when the app is launched from homescreen.

--- public/clientscripts/preventSafariOpen.js
+++ public/clientscripts/preventSafariOpen.js
@@ -1,17 +1,8 @@
 /**
  * Client script for preventing iOS devices to open links in Safari.
  *
- * @author ProfessorPotatis
+ * @author https://gist.github.com/kylebarrow/1042026
  * @version 1.0.0
  */
 
  (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener('click',function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;'href'in d&&(d.href.indexOf('http')||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href);},!1);}})(document,window.navigator,'standalone');
-
-/*let a = document.getElementsByTagName('a');
-for (let x = 0; x < a.length; x += 1)
-{
-    a[x].addEventListener('click', function() {
-        window.location = this.getAttribute('href');
-        return false;
-    });
-}*/
